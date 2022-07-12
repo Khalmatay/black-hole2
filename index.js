302cc44894a95cb8fe0d6ce91ca277a176672c94
Khalmatay <khalmatay@gmail.com>
2022-07-12 16:53:10 +0600

    animated star

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ let hole = new Image()
 
 star.src = "./images/star.png"
 hole.src = "./images/hole.png"
+
+var id = null;
+function myMove() {
+  var elem = document.getElementById("myAnimation");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
