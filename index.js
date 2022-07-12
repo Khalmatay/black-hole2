39dadcefa031ca4a56316637d97f0a195efe9a0c
Khalmatay <khalmatay@gmail.com>
2022-07-12 21:24:28 +0600

    conrol

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function myMove() {
     }
   }
 }
+
+var top = 0;
+window.onkeydown = function (event) {
+    var code = event.keyCode || event.charCode;
+    if (code != 38 && code != 40)
+        return;
+    var prop = document.getElementById('galax');
+    prop.style.top = prop.offsetTop+ (code == 38 ? -10 : 10) + 'px';
+};
+
+
+
+
+
