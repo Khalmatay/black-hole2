d6985bacdf6db14d7cd369d1ac651aa4a51b93ae
Khalmatay <khalmatay@gmail.com>
2022-07-13 10:41:21 +0600

    easy version

--- index.js
+++ index.js
@@ -4,37 +4,33 @@ const random = (min, max) => {
 k=random(1,720)
 console.log(random(1,10))
 
-var id = null;
-function myMove() {
-  // var elem = document.getElementById("stars");   
-  // var pos = 1800 ;
+// var id = null;
+// function myMove() {
+//   // var elem = document.getElementById("stars");   
+//   // var pos = 1800 ;
 
-  // clearInterval(id);
-  // id = setInterval(frame, 1);
-  // function frame() {
-  //   if (pos == 240) {
-  //     clearInterval(id);
-  //   } else {
-  //     pos--; 
-  //   //   elem.style.top = pos + 'px'; 
-  //     elem.style.left = pos +'px'; 
-  //   }
-  // }
+//   // clearInterval(id);
+//   // id = setInterval(frame, 1);
+//   // function frame() {
+//   //   if (pos == 240) {
+//   //     clearInterval(id);
+//   //   } else {
+//   //     pos--; 
+//   //   //   elem.style.top = pos + 'px'; 
+//   //     elem.style.left = pos +'px'; 
+//   //   }
+//   // }
 
-var boxElement = document.getElementById('stars');
-var animation = boxElement.animate([
-    {transform: 'translate(500)'},
-    {transform: `translate(-1600px, ${k}px) rotate(${k}deg)`}
-], 10000);
+// var boxElement = document.getElementById('stars');
+// var animation = boxElement.animate([
+//     {transform: 'translate(500)'},
+//     {transform: `translate(-1600px, ${k}px) rotate(${k}deg)`}
+// ], 10000);
 
-animation.addEventListener('finish', function() {
-boxElement.style.transform = `translate(-1600px, ${k}px) rotate(${k}deg)`;
-});
-}
-
-
-
-
+// animation.addEventListener('finish', function() {
+// boxElement.style.transform = `translate(-1600px, ${k}px) rotate(${k}deg)`;
+// });
+// }
 
 
 var top = 0;
